5925b0592c52ad32a8ac0f729c1f65bef33faaa3
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-26 21:24:45 +0700

    feat: implement DeploySC script

--- script/DeploySC.s.sol
+++ script/DeploySC.s.sol
@@ -4,12 +4,27 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {StableCoin} from "src/StableCoin.sol";
 import {SCEngine} from "src/SCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeploySC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (StableCoin, SCEngine) {
-        vm.startBroadcast();
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         StableCoin coin = new StableCoin();
-        // SCEngine engine = new SCEngine();
+        SCEngine engine = new SCEngine(tokenAddresses, priceFeedAddresses, address(coin));
+        coin.transferOwnership(address(engine));
         vm.stopBroadcast();
+
+        return (coin, engine);
     }
 }
