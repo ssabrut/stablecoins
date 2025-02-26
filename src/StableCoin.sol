1a8350350ef9edd541c753c5ba6102fbcb6f537f
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-26 18:36:20 +0700

    feat: implement StableCoin scaffolding:

---
+++ src/StableCoin.sol
@@ -0,0 +1,72 @@
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title StableCoin
+ * @author Michael Eco
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of stablecoin system.
+ */
+contract StableCoin is ERC20Burnable, Ownable {
+    error StableCoin__MustBeMoreThanZero();
+    error StableCoin__BurnAmountExceedsBalance();
+    error StableCoin__NotZeroAddress();
+
+    constructor() ERC20("StableCoin", "SC") Ownable(address(this)) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert StableCoin__MustBeMoreThanZero();
+        }
+
+        if (balance < _amount) {
+            revert StableCoin__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert StableCoin__NotZeroAddress();
+        }
+
+        if (_amount <= 0) {
+            revert StableCoin__MustBeMoreThanZero();
+        }
+
+        _mint(_to, _amount);
+        return true;
+    }
+}
